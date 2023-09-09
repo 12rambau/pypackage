5f49f147f48ae005dbd313666326975363068d69
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2023-09-09 09:44:41 +0200

    feat: first draft of copier implementation

--- {{cookiecutter.github_repo_name}}/tests/test_{{cookiecutter.project_slug}}.py
+++
@@ -1,8 +0,0 @@
-"""Test the {{ cookiecutter.project_slug }} package."""
-
-import {{ cookiecutter.project_slug }}
-
-
-def test_hello_world():
-    """Hello world test."""
-    assert {{ cookiecutter.project_slug }}.Hello().hello_world() == "hello world !"
