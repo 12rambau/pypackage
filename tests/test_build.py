10adb6500787c5a6c2250b33bf5655ac742ab203
Pierrick Rambaud <pierrick.rambaud@ldc.com>
2024-01-31 09:08:21 +0000

    fix: drop the citation.cff management

--- tests/test_build.py
+++ tests/test_build.py
@@ -66,17 +66,3 @@ def test_stub_file(copie: Copie, file_regression) -> None:
     subprocess.check_call(["nox", "-s" "stubgen"], cwd=result.project_dir)
     stub_path = Path(result.project_dir)/"stubs"/"package_skeleton"/"__init__.pyi"
     file_regression.check(stub_path.read_text(), extension=".pyi")
-
-
-def test_update_citation(copie: Copie) -> None:
-    """Emulate a release to update the release date in the citation file
-
-    Args:
-        copie: the copie to build
-    """
-    result = copie.copy()
-    subprocess.check_call(["nox", "-s", "release-date"], cwd=result.project_dir)
-
-    current_date = datetime.datetime.now().strftime("%Y-%m-%d")
-    citation_file = Path(result.project_dir)/"CITATION.cff"
-    assert f'date-released: "{current_date}"' in citation_file.read_text()
